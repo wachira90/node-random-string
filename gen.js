6b2661f48aa7690944fd62c4006fb55e7c231845
Wachira Duangdee <wcrcorp@gmail.com>
2021-04-10 18:50:33 +0700

    Create gen.js

---
+++ gen.js
@@ -0,0 +1,12 @@
+function gentoken(length) {
+    var result = [];
+    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    // var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for (var i = 0; i < length; i++) {
+        result.push(characters.charAt(Math.floor(Math.random() * charactersLength)));
+    }
+    return result.join('');
+}
+
+console.log(gentoken(6));
